6f8ceee8770e7683bc3f5f0107b01eed3195ec15
davidwinters <mac@mpcstudios.com>
2013-03-20 00:12:50 -0500

    fiddling with the ui

--- sketchdaily/sketchdaily/settings.py
+++ sketchdaily/sketchdaily/settings.py
@@ -102,12 +102,9 @@ ROOT_URLCONF = 'sketchdaily.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'sketchdaily.wsgi.application'
 
-TEMPLATE_DIRS = (
-    "D:/src/github/SketchDaily-GestureDrawing/sketchdaily/templates"
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+import os
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/'),)
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
