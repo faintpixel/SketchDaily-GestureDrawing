42e0a772d916850bd9e9fe13f971e2ce2a3b6df8
davidwinters <mac@mpcstudios.com>
2013-03-29 22:12:03 -0500

    OOPS

--- sketchdaily/sketchdaily/settings.py
+++ sketchdaily/sketchdaily/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'skdgesture',                      # Or path to database file if using sqlite3.
         'USER': 'drawbot',                      # Not used with sqlite3.
-        'PASSWORD': 'rspj2005',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'mysql.newfrost.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
